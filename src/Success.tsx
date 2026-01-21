89d97c363bb87f24feb6808cab59d641ab89864b
kuagabriel8 <kuagabriel8@gmail.com>
2026-01-21 21:22:36 +0800

    add confetti

--- src/Success.tsx
+++ src/Success.tsx
@@ -1,7 +1,15 @@
 import './App.css'
 import pochaccoGif from "./assets/pochacco-icegif-61-ezgif.com-loop-count.gif";
+import confetti from "canvas-confetti";
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+    // a few bursts looks nicer than one big blast
+    confetti({ particleCount: 120, spread: 70, origin: { y: 0.6 } });
+    confetti({ particleCount: 80, spread: 90, origin: { y: 0.8 } });
+  }, []);
+
 
   return (
     <div className="container">
